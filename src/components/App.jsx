d5c44cb180820e54b42dfc6cc5f30af43d29d9ea
antwonlee <antwonlee@gmail.com>
2016-05-31 14:56:57 -0500

    Add  separate components to App :dizzy_face:
    Separate one big chunk of code to small components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
+import MessageList from './MessageList.jsx';
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {
-      messages: [
-        'Hello there how are you?',
-        'I am fine, and you?'
-      ]
-    };
   }
 
   render() {
-    debugger;
-    var messageNodes = this.state.messages.map((messages)=> {
-      return (
-        <div>{messages}</div>
-      );
-    });
-
     return (
-      <div>{messageNodes}</div>
-    );
+      <MessageList />
+    )
   }
 }
 
